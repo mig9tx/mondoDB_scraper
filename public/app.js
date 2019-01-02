f21ff560745fa84cc5cf3f40686705f6e234cb33
mig9tx <migue.trujillo@utexas.edu>
2019-01-02 11:07:47 -0600

    styling and minor tweaks

--- public/app.js
+++ public/app.js
@@ -62,7 +62,7 @@ $(document).ready(function () {
 
     //Click event to open note modal and show notes
     $(".addNote").on("click", function (event) {
-
+        $(".modal-footer").empty();
         $("#noteArea").empty();
         let thisId = $(this).attr("article_id");
         console.log(thisId + "line 68 app.js")
@@ -74,13 +74,19 @@ $(document).ready(function () {
                 console.log(data);
                 $(".modal-title").text(data.title);
                 console.log(data._id);
-                $("#deleteNote").attr("article_id", data._id);
-                $("#saveNote").attr("article_id", data._id);
+                // $("#deleteNote").attr("article_id", data._id);
                 if (data.note) {
-                    console.log(data.note.title);
+                    console.log("note_id " + data.note._id);
+                    $(".modal-footer").append("<button type='button' class='btn btn-success' data-dismiss='modal' id='saveNote'>Save Note</button>");
+                    $("#saveNote").attr("article_id", data._id);
+                    $(".modal-footer").append("<button type='button' class='btn btn-danger' data-dismiss='modal' id='deleteNote'>Delete Note</button>");
+                    $("#deleteNote").attr("article_id", data._id);
                     $("#noteArea").append("<h4>Title: " + data.note.title + "</h4>");
                     $("#noteArea").append("<p>Note: " + data.note.body + "</p>");
-                }
+                }else {
+                $(".modal-footer").append("<button type='button' class='btn btn-success' data-dismiss='modal' id='saveNote'>Save Note</button>");
+                $("#saveNote").attr("article_id", data._id);
+                };
             });
     });
 
@@ -103,8 +109,8 @@ $(document).ready(function () {
                 // Log the response
                 console.log(data);
             });
-            $("#noteTitle").val("");
-            $("#noteBody").val("");
+        $("#noteTitle").val("");
+        $("#noteBody").val("");
     });
 
     //Click event to delete a note for an article
@@ -113,14 +119,13 @@ $(document).ready(function () {
         console.log(thisId);
 
         $.ajax({
-            method: "GET",
-            url: "/articles/" + thisId,
-        })
-        .then (function (data){
-            console.log(data.note)
-            console.log(data);
-        }
-        )
+                method: "GET",
+                url: "/articles/" + thisId,
+            })
+            .then(function (data) {
+                console.log(data.note)
+                console.log(data);
+            })
     })
 
 
